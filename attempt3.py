1032a7743c4ffa86fccc3c01576be9ce7ef57e1e
Niels Smith <30351969+smithniels@users.noreply.github.com>
2019-08-05 07:48:50 -0400

    Update attempt3.py

--- attempt3.py
+++ attempt3.py
@@ -1,24 +1,19 @@
 '''
 Attempt #3
-https://github.com/realpython/book2-exercises/tree/master/flask-blog
-http://www.sqlitetutorial.net/sqlite-python/sqlite-python-select/
-https://stackoverflow.com/questions/16311974/connect-to-a-database-in-flask-which-approach-is-better
-K, I'M HIGH,
-BUT_REVIEW_THOSE(3_LINKS^)
-IF DATABASE_CONNECTION = FALSE:
-    RETURN REDO_ALL_DATABASE_CONNECTION_STUFF()
-ELSE:
-    RETURN CELEBRATE()
 
-
-TODO: Add in a check if logged in - else --> redirect(url_for(/login))
-        JK that already exists. I think I need an auto logout like session.pop(logged_in, none) but automatically
-            maybe put it at the start of every refresh, but that might get annoying
-                can you time it somehow? At what point would you stop?
-                    what are the common rules here?
-TODO: Figure out flask_login
 TODO: Figure out how to return the username i.e. "you are logged in as {{username}}"
-TODO: Add timestamp to posts
+TODO: Add autocrement ID to db
+TODO: Figure out flask_login
+TODO: Add in a check if logged in - else --> redirect(url_for(/login))
+            JK that already exists. I think I need an auto logout like session.pop(logged_in, none) but automatically
+            maybe put it at the start of every refresh, but that might get annoying
+            can you time it somehow? At what point would you stop? What are the rules here?
+TODO: Make it so CTRL+ENT will just post. (is that a good idea?)
+TODO: Add an ABOUT ME page
+TODO: Make it pretty
+TODO: Add in an edit button
+TODO: Add in a delete button
+TODO: Connect to www.nielssmith.com
 
 '''
 
@@ -34,6 +29,7 @@ import os
 import mysql.connector
 import sqlite3
 import MySQLdb
+from datetime import datetime
 
 app = Flask(__name__) # instantiate object
 Bootstrap(app) # Make app bootstrap enabled
@@ -43,14 +39,14 @@ mysql  = MySQL(app)
 
 #Config
 db = yaml.full_load(open('db.yaml')) #someone on stack said use 'full_load' not 'load'
-app.config['MYSQL_HOST'] = db['mysql_host']
-app.config['MYSQL_DB'] = db['mysql_db']
-app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_HOST']     = db['mysql_host']
+app.config['MYSQL_DB']       = db['mysql_db']
+app.config['MYSQL_USER']     = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
-app.config['USERNAME'] = db['username']
-app.config['PASSWORD'] = db['password']
-app.config['SECRET_KEY'] = db['secret_key']
-mysql = MySQL(app)
+app.config['USERNAME']       = db['username']
+app.config['PASSWORD']       = db['password']
+app.config['SECRET_KEY']     = db['secret_key']
+mysql                        = MySQL(app)
 # print(os.getcwd())
 #
 # function used for connecting to the database
@@ -58,7 +54,7 @@ mysql = MySQL(app)
 def connect_db():
     return MySQLdb.Connection(host = app.config['MYSQL_HOST'],
                               user = app.config['MYSQL_USER'],
-                              passwd =app.config['MYSQL_PASSWORD'],
+                              passwd = app.config['MYSQL_PASSWORD'],
                               database = app.config['MYSQL_DB']
                               )
 
@@ -78,7 +74,7 @@ def login():
     status_code = 200
     if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
-            error = 'you are invalid'
+            error = 'You are invalid'
             status_code = 401
         else:
             session['logged_in'] = True
@@ -90,26 +86,31 @@ def login():
 def main():
     g.db = connect_db()
     cur = g.db.cursor()
-    cur.execute('select * from user')  # Problem <<<<<< AttributeError: 'Connection' object has no attribute 'execute'
-    # posts = [dict(title=row[0], post=row[1]) for row in cur.fetchall()] # No, I don't fully understand this. Why do you ask?
-    texttext = cur.fetchall()
-    # return str(texttext)
-    return render_template('main.html')#, posts=posts)
+    cur.execute('select * from user')
+    posts = [dict(title=row[0], post=row[1], time= row[3]) for row in cur.fetchall()] # No, I don't fully understand this. Why do you ask?
+                                                                                      #  
+                                                                                      # It's looking row by row through the db and returning 
+                                                                                      # the values based on their position and placing them in 
+                                                                                      # a dictionary 
+    g.db.close()
+    return render_template('main.html', posts=posts)
 
 @app.route('/add', methods=['POST'])
 @login_required
 def add():
     title = request.form['title']
     post = request.form['post']
-    if not title or not post: # If blank return error
+    time = datetime.today()
+    if not title or not post:
         flash("All fields are required. Please try again.",'error')
         return redirect(url_for('main'))
     else:
         g.db = connect_db()
         cur = g.db.cursor()
-        cur.execute('''INSERT INTO user (title, posts) VALUES (%s ,%s) ''' , (title,post)
-         # (?, ?)', [request.form['title'], request.form['post']]
-        )
+        cur.execute('''INSERT INTO user (title, posts, time)
+                       VALUES (%s ,%s, %s)
+                    ''' , (title,post,time)
+                    )
         cur.close()
         g.db.commit()
         cur.close()
@@ -118,7 +119,7 @@ def add():
 
 @app.route('/logout')
 def logout():
-    session.pop('logged_in', None) # To release a session variable use pop() method!!!
+    session.pop('logged_in', None) # To release a session use pop() method!!!
     flash('You were logged out')
     return redirect(url_for('login'))
 
@@ -128,18 +129,12 @@ if __name__ == '__main__':
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
+# https://github.com/realpython/book2-exercises/tree/master/flask-blog
+# http://www.sqlitetutorial.net/sqlite-python/sqlite-python-select/
+# https://stackoverflow.com/questions/16311974/connect-to-a-database-in-flask-which-approach-is-better
+# K, I'M HIGH,
+#     BUT_REVIEW_THOSE(3_LINKS^)
+#     IF DATABASE_CONNECTION = FALSE:
+#     ELSE:
+#         RETURN REDO_ALL_DATABASE_CONNECTION_STUFF()
+#         RETURN CELEBRATE()
